c9143e15f05f9ff4a0360547a568ee046397220c
Vikash <phanivikash2@gmail.com>
2025-02-16 22:09:00 +0530

    Contact_me page completed

--- pages/Contact_me.py
+++ pages/Contact_me.py
@@ -1,9 +1,21 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact me")
 
 with st.form(key='email'):
     email_id=st.text_input("Your E-mail")
-    mes=st.text_area("Enter your message ")
+    sub = st.text_input("Subject of email")
+    raw_message=st.text_area("Enter your message ")
+
+    message=f'''\
+Subject : {sub}
+
+from: {email_id}    
+{raw_message}'''
+
     button=st.form_submit_button("Submit")
 
+    if button:
+        send_email(message)
+        st.info("Your email sent successfully")
--- send_email.py
+++ send_email.py
@@ -1,18 +1,17 @@
 import smtplib , ssl
 
-host='smtp.gmail.com'
-port=465
 
-username='phanivikash@gmail.com'
-password='cnrq wzsf pyuo dodq'
-receiver='phanivikash2@gmail.com'
-mess='''\
-Subject : Testing
-Hi ,SMTP Testing
-'''
+def send_email(mess):
+    host='smtp.gmail.com'
+    port=465
 
-content=ssl.create_default_context()
+    username='phanivikash@gmail.com'
+    password='cnrq wzsf pyuo dodq'
+    receiver='phanivikash@gmail.com'
 
-with smtplib.SMTP_SSL(host,port,context=content) as server:
-    server.login(user=username,password=password)
-    server.sendmail(username,receiver,msg=mess)
+
+    content=ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host,port,context=content) as server:
+        server.login(user=username,password=password)
+        server.sendmail(username,receiver,msg=mess)
